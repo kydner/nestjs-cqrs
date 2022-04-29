cde2a59c1750061e72e58c4613a23e3a0506cc7b
Muhammad Rendika Purnomo <muhammad.purnomo@dwidasa.com>
2022-04-29 16:38:34 +0700

    global api

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
     }),
   );
 
+  app.setGlobalPrefix('api');
+
   const config = new DocumentBuilder()
     .setTitle('Nestjs example')
     .setDescription('The cats API description')
